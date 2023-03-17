f6adf12da084f98bfa13fcc4aa81a76e986a0fad
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-03-17 23:24:18 +0300

    Lesson27 v4.2

--- src/main/java/ru/otus/lesson27/config/SecurityConfig.java
+++ src/main/java/ru/otus/lesson27/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package ru.otus.lesson27.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,13 +11,10 @@ import ru.otus.lesson27.service.UserService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserService userService;
-
-    public SecurityConfig(UserService userService) {
-        this.userService = userService;
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,16 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .permitAll()
                 );
     }
-
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(userService);
-        return authenticationProvider;
-    }
-
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
