6f9ba5f1911a633067fcfcedb5d62c435b0311b1
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2023-03-17 15:52:20 +0300

    Lesson27 v2

--- src/main/java/ru/otus/lesson27/config/SecurityConfig.java
+++ src/main/java/ru/otus/lesson27/config/SecurityConfig.java
@@ -21,7 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
+                .cors().disable()
                 .authorizeRequests()
                 .antMatchers("/api/**").hasRole("USER")
                 .and()
